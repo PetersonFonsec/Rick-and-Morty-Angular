ee8b4bb703ddebaab57713ebeaf3c47cb94bd1d4
Peterson <petersonsimiao1@gmail.com>
2021-09-26 12:15:08 -0300

    test(FormSeachComponent): Criado test para os @Output

--- src/app/shared/components/form-search/form-search.component.spec.ts
+++ src/app/shared/components/form-search/form-search.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { FormSearchComponent } from './form-search.component';
 import { FormSearchModule } from './form-search.module';
@@ -21,5 +26,36 @@ describe('FormSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should trigger (@Output search) when input', fakeAsync(() => {
+    let times = 0;
+    component.search.subscribe(() => times++);
+    const search = component.formGroup.get('search');
+    search?.setValue('test');
+    tick(500);
+    expect(times).toEqual(1);
+  }));
+
+  it('Should trigger (@Output search) only one time when input very times in debouce time ', fakeAsync(() => {
+    let times = 0;
+    component.search.subscribe(() => times++);
+    const search = component.formGroup.get('search');
+    search?.setValue('t');
+    search?.setValue('e');
+    search?.setValue('s');
+    search?.setValue('t');
+    tick(500);
+    expect(times).toEqual(1);
+  }));
+
+  it('Should emit (@Output search) with value to input search', (done) => {
+    const searchValue = 'morty';
+    component.search.subscribe((result) => {
+      expect(result).toEqual(searchValue);
+      done();
+    });
+    const search = component.formGroup.get('search');
+    search?.setValue(searchValue);
   });
 });
