41e582a4a25d6ce4ab26ec65f644de7edb08fe2d
Peterson <petersonsimiao1@gmail.com>
2021-10-01 00:32:33 -0300

    feat(Episode Model): Criado componente modal

--- src/app/episode/episode.module.ts
+++ src/app/episode/episode.module.ts
@@ -9,9 +9,12 @@ import { EpisodeService } from './services/episode/episode.service';
 import { FormSearchModule } from '../shared/components/form-search/form-search.module';
 import { CardModule } from '../shared/components/card/card.module';
 import { ListModule } from '../shared/components/list/list.module';
+import { ModalComponent } from './components/modal/modal.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ButtonModule } from '../shared/components/button/button.module';
 
 @NgModule({
-  declarations: [EpisodeComponent],
+  declarations: [EpisodeComponent, ModalComponent],
   imports: [
     CommonModule,
     EpisodeRoutingModule,
@@ -20,6 +23,8 @@ import { ListModule } from '../shared/components/list/list.module';
     ReactiveFormsModule,
     CardModule,
     ListModule,
+    MatDialogModule,
+    ButtonModule,
   ],
   providers: [EpisodeService],
 })
