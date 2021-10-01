4b21fe2e8078b4203c944f7a739d680c1ac738da
Peterson <petersonsimiao1@gmail.com>
2021-10-01 00:33:27 -0300

    refactor(shared model): Removido impor do componente button

--- src/app/shared/components/card/card.module.ts
+++ src/app/shared/components/card/card.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
-import { ButtonModule } from '../button/button.module';
 
 @NgModule({
   declarations: [CardComponent],
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule],
   exports: [CardComponent],
 })
 export class CardModule {}
