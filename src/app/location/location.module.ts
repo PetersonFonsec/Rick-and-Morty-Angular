c6020c7825fb1b544d763cf5fdf91c239c25137b
Peterson <petersonsimiao1@gmail.com>
2021-10-09 13:07:19 -0300

    refactor(Location Module): Fazendo a busca com base em queryString

--- src/app/location/location.module.ts
+++ src/app/location/location.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { LocationRoutingModule } from './page/location-routing.module';
 import { LocationComponent } from './page/location.component';
-import { FormSearchModule } from '../shared/components/form-search/form-search.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
 import { CardModule } from '../shared/components/card/card.module';
 import { LocationService } from './services/location/location.service';
@@ -18,7 +17,6 @@ import { MatDialogModule } from '@angular/material/dialog';
   imports: [
     CommonModule,
     LocationRoutingModule,
-    FormSearchModule,
     LoadingModule,
     CardModule,
     HttpClientModule,
--- src/app/location/page/location.component.ts
+++ src/app/location/page/location.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ModalComponent } from '../components/modal/modal.component';
 import { Location } from '../services/location/location.model';
 import { LocationService } from '../services/location/location.service';
@@ -9,43 +11,66 @@ import { LocationService } from '../services/location/location.service';
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.scss'],
 })
-export class LocationComponent implements OnInit {
+export class LocationComponent implements OnInit, OnDestroy {
   locations: Location[] = [];
+  subscriptionGet!: Subscription;
+  subscriptionParam!: Subscription;
+  subscriptionSeach!: Subscription;
   locationName = '';
   loading = true;
 
   constructor(
     private locationService: LocationService,
+    private activatedRoute: ActivatedRoute,
     private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
     this.get();
+    this.handleQueryParams();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionGet?.unsubscribe();
+    this.subscriptionParam?.unsubscribe();
+    this.subscriptionSeach?.unsubscribe();
+  }
+
+  handleQueryParams() {
+    this.subscriptionParam = this.activatedRoute.queryParams.subscribe(
+      (query) => {
+        query?.name ? this.search(query.name) : this.get();
+      }
+    );
   }
 
   get() {
     this.loading = true;
 
-    this.locationService.getAll().subscribe((response) => {
-      this.loading = false;
-      this.locations = response.results;
-    });
+    this.subscriptionGet = this.locationService
+      .getAll()
+      .subscribe((response) => {
+        this.loading = false;
+        this.locations = response.results;
+      });
   }
 
   search(name: string) {
     this.loading = true;
     this.locationName = name;
 
-    this.locationService.getByQuery({ name }).subscribe(
-      (response) => {
-        this.loading = false;
-        this.locations = response.results;
-      },
-      () => {
-        this.loading = false;
-        this.locations = [];
-      }
-    );
+    this.subscriptionSeach = this.locationService
+      .getByQuery({ name })
+      .subscribe(
+        (response) => {
+          this.loading = false;
+          this.locations = response.results;
+        },
+        () => {
+          this.loading = false;
+          this.locations = [];
+        }
+      );
   }
 
   visualize(data: Location): void {
